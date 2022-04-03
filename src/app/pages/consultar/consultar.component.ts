7dc9abd611e1b011430954f6a5555908021b342d
unknown <erikmaquera@gmail.com>
2022-04-03 16:01:32 -0400

    package apr 22

--- src/app/pages/consultar/consultar.component.ts
+++ src/app/pages/consultar/consultar.component.ts
@@ -1,9 +1,13 @@
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 
 import { ConductorService } from '../../services/conductor.service';
+
+//reportes
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
 
 import swal from 'sweetalert';
 import { AntecedentesService } from '../../services/antecedentes.service';
@@ -101,22 +105,29 @@ export class ConsultarComponent implements OnInit {
   modificar(value){
     alert("modificar"+value);
   }
+
+  downloadPDF(){
+    const DATA = document.getElementById('htmlData');
+    const doc = new jsPDF('p', 'pt', 'a4');
+    const options = {
+      background: 'white',
+      scale: 3
+    };
+    html2canvas(DATA, options).then((canvas) => {
+
+      const img = canvas.toDataURL('image/PNG');
+
+      // Add image Canvas to PDF
+      const bufferX = 15;
+      const bufferY = 15;
+      const imgProps = (doc as any).getImageProperties(img);
+      const pdfWidth = doc.internal.pageSize.getWidth() - 2 * bufferX;
+      const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+      doc.addImage(img, 'PNG', bufferX, bufferY, pdfWidth, pdfHeight, undefined, 'FAST');
+      return doc;
+    }).then((docResult) => {
+      docResult.save(`conductores.pdf`);
+    });
+  }
 }
 
-
-
-/** Builds and returns a new User. */
-// function createNewUser(id: number): UserData {
-//   const name =
-//     NAMES[Math.round(Math.random() * (NAMES.length - 1))] +
-//     ' ' +
-//     NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) +
-//     '.';
-
-//   return {
-//     id: id.toString(),
-//     name: name,
-//     progress: Math.round(Math.random() * 100).toString(),
-//     fruit: FRUITS[Math.round(Math.random() * (FRUITS.length - 1))],
-//   };
-// }
